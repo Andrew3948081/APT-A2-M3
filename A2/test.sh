2a864532622a8ea4bda675bb35c23ca8538a879a
Andrew3948081 <s3948081@student.rmit.edu.au>
2023-05-25 13:31:21 +1000

    tests

--- A2/test.sh
+++ A2/test.sh
@@ -1,12 +1,21 @@
 #! /usr/bin/bash
 
-
+#tests 50+ are for modified program 
 for i in {9..50}
 do
     #i=50
     testNum=test$i
-    #echo $testNum
-    ./ppd stock.dat coins.dat < tests/$testNum.input > tests/$testNum.actual_ppd_out true
+    echo $testNum
+    ./ppd stock.dat coins.dat < tests/$testNum.input > tests/$testNum.actual_ppd_out false
+    diff  -w tests/$testNum.output tests/$testNum.actual_ppd_out
+done
+
+
+for i in {51..70}
+do
+    testNum=test$i
+    echo $testNum
+    ./ppd stock_new.sav coins.dat < tests/$testNum.input > tests/$testNum.actual_ppd_out true
     diff  -w tests/$testNum.output tests/$testNum.actual_ppd_out
 done
 
